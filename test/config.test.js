54c271f956478456bd18ba064ba14bdc6fe695b7
Sitraka Ratsimba <sitraka.harry@gmail.com>
2017-03-28 15:25:03 +0300

    Add function get config

--- test/config.test.js
+++ test/config.test.js
@@ -1,12 +1,25 @@
 'use strict';
 
+const Config = require('../lib/config');
+const Schema = require('../lib/schema');
+
 describe('Config', () => {
 
     describe('#get()', () => {
 
         it('Return default options', () => {
 
-            expect(1+1).toBe(3);
+            const defaultConf = {
+                dir: __dirname + '/routes',
+                pattern: '/**/!(_)*.js'
+            };
+            expect(Config.get({ dir: __dirname + '/routes' }, Schema.plugin))
+                .toEqual(defaultConf);
+        });
+
+        it('Thow an expection when options.dir is not set', () => {
+
+            expect(() => Config.get({}, Schema.plugin)).toThrow();
         });
     });
 });
