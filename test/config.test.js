00d736788a2596a143f067255e7777670ff69bd3
Sitraka Ratsimba <sitraka.harry@gmail.com>
2017-03-28 15:01:23 +0300

    Initialize tests

---
+++ test/config.test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+describe('Config', () => {
+
+    describe('#get()', () => {
+
+        it('Return default options', () => {
+
+            expect(1+1).toBe(3);
+        });
+    });
+});
