48d4243db9b50d3d2336c777908fbbb6900cb477
Sitraka Ratsimba <sitraka.harry@gmail.com>
2017-04-02 21:38:47 +0300

    Replace jest by lab

---
+++ test/options.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const Code = require('code');   // assertion library
+const Lab = require('lab');
+const lab = exports.lab = Lab.script();
+
+const Options = require('../lib/options');
+
+lab.describe('Options', (done) => {
+
+    lab.describe('#getOptions()', () => {
+
+        lab.it('applies defaults value', (done) => {
+
+            const defaultOptions = Options.plugin({
+                dir: __dirname + '/routes'
+            });
+            Code.expect(defaultOptions).to.be.equal({
+                dir: __dirname + '/routes',
+                pattern: '/**/!(_)*.js',
+                prefix: true
+            });
+            done();
+        });
+
+        lab.it('throws exception when dir is not set', (done) => {
+
+            Code.expect(() => {
+
+                Options.plugin({});
+            }).to.throw();
+            done();
+        });
+    });
+});
--- test/routes/api/login.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = {
-    method: 'GET',
-    path: '/login',
-    handler: (request, response) => reply('/api/login')
-};
