fc544710d0be2aa4fff090ecd04e021258c0681f
Sitraka Ratsimba <sitraka.harry@gmail.com>
2017-04-16 19:21:25 +0300

    Fix bug #1

--- test/prefix.js
+++ test/prefix.js
@@ -28,5 +28,14 @@ lab.describe('Prefix', () => {
             Code.expect(Prefix.add(dir, file, path)).to.be.equal('/a/b/c');
             done();
         });
+
+        lab.it('Remove prefix in the root dir', (done) => {
+
+            const dir = '/path/to/route';
+            const file = '/path/to/route/login.js';
+            const path = '/login';
+            Code.expect(Prefix.add(dir, file, path)).to.be.equal('/login');
+            done();
+        });
     });
 });
