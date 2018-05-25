da09b4adf915998f286a5cdedf64c2775e453d4e
Sitraka Ratsimba <ratsimbasitraka@gmail.com>
2018-05-25 21:49:09 +0300

    Refactor tests

--- test/plugin_test.js
+++ test/plugin_test.js
@@ -8,9 +8,28 @@ const lab = exports.lab = require('lab').script();
 
 lab.experiment('plugin', () => {
 
+    let server = null;
+    let plugin = null;
+    let hapiAutoRouteOptions = null;
+
+    lab.beforeEach(() => {
+
+        server = Hapi.Server({ port: 3000 });
+        plugin = HapiAutoRoute.plugin;
+        hapiAutoRouteOptions = {
+            routes_dir: 'test/fixtures/routes',
+            pattern: '**/*.js',
+            use_prefix: true
+        };
+    });
+
+    lab.afterEach(() => {
+
+        server = null;
+    });
+
     lab.it('pluging.pkg should set with package.json', () => {
 
-        const plugin = HapiAutoRoute.plugin;
         expect(plugin).to.not.undefined();
         expect(plugin.pkg).to.not.undefined();
         expect(plugin.pkg).include(['name', 'version', 'description']);
@@ -18,20 +37,16 @@ lab.experiment('plugin', () => {
 
     lab.it('Should have register function', () => {
 
-        const plugin = HapiAutoRoute.plugin;
         expect(plugin.register).to.not.undefined();
         expect(plugin.register).to.be.function();
     });
 
     lab.it('Register route to the server', async () => {
 
-        const server = Hapi.Server({ port: 3000 });
+        hapiAutoRouteOptions.use_prefix = false;
         await server.register({
             plugin: HapiAutoRoute,
-            options: {
-                routes_dir: 'test/fixtures/routes',
-                pattern: '**/!(_)*.js'
-            }
+            options: hapiAutoRouteOptions
         });
         const result = await server.inject('/');
         expect(result.statusCode).to.be.equal(200);
@@ -39,12 +54,11 @@ lab.experiment('plugin', () => {
 
     lab.it('Apply default options', async () => {
 
-        const server = Hapi.Server({ port: 3000 });
+        delete hapiAutoRouteOptions.use_prefix;
+        delete hapiAutoRouteOptions.pattern;
         await server.register({
             plugin: HapiAutoRoute,
-            options: {
-                routes_dir: 'test/fixtures/routes'
-            }
+            options: hapiAutoRouteOptions
         });
         const result = await server.inject('/');
         expect(result.statusCode).to.be.equal(200);
@@ -53,13 +67,9 @@ lab.experiment('plugin', () => {
 
     lab.it('Change route path if use_prefix is true', async () => {
 
-        const server = Hapi.Server({ port: 3000 });
         await server.register({
             plugin: HapiAutoRoute,
-            options: {
-                routes_dir: 'test/fixtures/routes',
-                use_prefix: true
-            }
+            options: hapiAutoRouteOptions
         });
         const result = await server.inject('/auth/login');
         expect(result.statusCode).to.be.equal(200);
