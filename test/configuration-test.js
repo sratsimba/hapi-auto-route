820f52e44647aed0c7b5d003b57fdcb129094bfc
Sitraka Ratsimba <ratsimbasitraka@gmail.com>
2018-05-30 01:24:58 +0300

    Refactor

--- test/configuration-test.js
+++ test/configuration-test.js
@@ -4,75 +4,77 @@ const Lab = require('lab');
 
 const { expect } = require('code');
 
-// The hapi-auto-route plugin
 const { Configuration } = require('../index.js');
 
 const lab = exports.lab = Lab.script();
 
-lab.describe('Option', () => {
+lab.describe('Configuration', () => {
 
-    lab.it('Options is an object', () => {
+    lab.it('is a class', () => {
 
         expect(Configuration).to.be.not.undefined()
             .and.to.be.an.function(); // a class.
     });
 
-    lab.it('Has a merged property of routerSetting and options', () => {
+    lab.describe('#constructor()', () => {
 
-        const routerSetting = {
-            isCaseSensitive: true,
-            stripTrailingSlash: false
-        };
-        const options = {
-            routes_dir: 'sd',
-            pattern: 'df',
-            use_prefix: false
-        };
-        const config = new Configuration(routerSetting, options);
-        expect(config.router).to.be.an.object().and.include([
-            'isCaseSensitive', 'stripTrailingSlash'
-        ]);
-        expect(config.plugin).to.be.an.object().and.include([
-            'routes_dir', 'pattern', 'use_prefix'
-        ]);
-    });
+        lab.it('stores router settings and plugin options', () => {
 
-    lab.it('Throw an error when options is not valid', () => {
+            const routerSetting = {
+                isCaseSensitive: true,
+                stripTrailingSlash: false
+            };
+            const options = {
+                routes_dir: 'sd',
+                pattern: 'df',
+                use_prefix: false
+            };
+            const config = new Configuration(routerSetting, options);
+            expect(config.router).to.be.an.object().and.include([
+                'isCaseSensitive', 'stripTrailingSlash'
+            ]);
+            expect(config.plugin).to.be.an.object().and.include([
+                'routes_dir', 'pattern', 'use_prefix'
+            ]);
+        });
 
-        const options = {
-            routes_dir: 34, // should be a string
-            pattern: 55, // should be a string
-            use_prefix: 'blabla' // should be a boolean (true or false)
-        };
+        lab.it('ensure that plugin options are valid', () => {
 
-        expect(() => {
+            const options = {
+                routes_dir: 34, // should be a string
+                pattern: 55, // should be a string
+                use_prefix: 'blabla' // should be a boolean (true or false)
+            };
 
-            new Configuration(null, options);
-        }).to.throw();
-    });
+            expect(() => {
 
-    lab.it('Throws an error when router settings are invalid', () => {
+                new Configuration(null, options);
+            }).to.throw();
+        });
 
-        const options = {
-            routes_dir: 'sd',
-            pattern: 'df',
-            use_prefix: false
-        };
-        expect(() => {
+        lab.it('ensure that router settings are valid', () => {
 
-            new Configuration({}, options);
-        }).to.throw();
-    });
+            const options = {
+                routes_dir: 'sd',
+                pattern: 'df',
+                use_prefix: false
+            };
+            expect(() => {
 
-    lab.it('applies default plugin options when "options" is undifined.', () => {
+                new Configuration({}, options);
+            }).to.throw();
+        });
 
-        const routerSetting = {
-            isCaseSensitive: true,
-            stripTrailingSlash: false
-        };
-        const config = new Configuration(routerSetting, {});
-        expect(config.plugin.routes_dir).to.be.equal('routes');
-        expect(config.plugin.pattern).to.be.equal('**/*.js');
-        expect(config.plugin.use_prefix).to.be.false();
+        lab.it('applies default plugin options when they are not specified.', () => {
+
+            const routerSetting = {
+                isCaseSensitive: true,
+                stripTrailingSlash: false
+            };
+            const config = new Configuration(routerSetting, {});
+            expect(config.plugin.routes_dir).to.be.equal('routes');
+            expect(config.plugin.pattern).to.be.equal('**/*.js');
+            expect(config.plugin.use_prefix).to.be.false();
+        });
     });
 });
